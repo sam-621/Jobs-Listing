0b1f91f32f6978b8953b9a1e08a4b05e468b67eb
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-07-03 14:57:05 -0600

    Updates

---
+++ precache-manifest.9efcda0ccd7ab874e112eebd3e6f23a1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ae237d1b5b0f66cf6db4b52216ad0d",
+    "url": "/Jobs-Listing/index.html"
+  },
+  {
+    "revision": "cc12dc7962c5754a4e8f",
+    "url": "/Jobs-Listing/static/css/main.a9381093.chunk.css"
+  },
+  {
+    "revision": "9f80964beb5db096534c",
+    "url": "/Jobs-Listing/static/js/2.da9ea526.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jobs-Listing/static/js/2.da9ea526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc12dc7962c5754a4e8f",
+    "url": "/Jobs-Listing/static/js/main.58aed5aa.chunk.js"
+  },
+  {
+    "revision": "d2348197ac315f746c8e",
+    "url": "/Jobs-Listing/static/js/runtime-main.6f27d876.js"
+  },
+  {
+    "revision": "9f72c13bc10a76e4bcb8f7ab62582b9f",
+    "url": "/Jobs-Listing/static/media/account.9f72c13b.svg"
+  },
+  {
+    "revision": "6e2f7196c0644447b6887a48a265bdb1",
+    "url": "/Jobs-Listing/static/media/bg-header-desktop.6e2f7196.svg"
+  },
+  {
+    "revision": "39eaebdf1b82df9648392a9fb0915e9c",
+    "url": "/Jobs-Listing/static/media/eyecam-co.39eaebdf.svg"
+  },
+  {
+    "revision": "76faa79fba7225014706c0aac0b46472",
+    "url": "/Jobs-Listing/static/media/faceit.76faa79f.svg"
+  },
+  {
+    "revision": "52b85a5018745489a50626498bc347ad",
+    "url": "/Jobs-Listing/static/media/insure.52b85a50.svg"
+  },
+  {
+    "revision": "6c50fd745d8d5c5e1dd47fa8c9029a6b",
+    "url": "/Jobs-Listing/static/media/loop-studios.6c50fd74.svg"
+  },
+  {
+    "revision": "fbe431a9e5d624447d698fee099ca843",
+    "url": "/Jobs-Listing/static/media/manage.fbe431a9.svg"
+  },
+  {
+    "revision": "e0ad9358d008658c14d4350d48627923",
+    "url": "/Jobs-Listing/static/media/myhome.e0ad9358.svg"
+  },
+  {
+    "revision": "3f34b49c96dcbb44450b43184cd6cd78",
+    "url": "/Jobs-Listing/static/media/photosnap.3f34b49c.svg"
+  },
+  {
+    "revision": "45f63772b29b7ccba3bc0c0c6e1d505d",
+    "url": "/Jobs-Listing/static/media/shortly.45f63772.svg"
+  },
+  {
+    "revision": "23b515f95978bcb24d704b1abecc8196",
+    "url": "/Jobs-Listing/static/media/the-air-filter-company.23b515f9.svg"
+  }
+]);
